ea2e6f3841746b200370d466fef9e24e13ccea97
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-31 12:59:01 +0000

    Flask-SQLAlchemy and Flask-Migrate initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 """Flask application instance """
-from flask import Flask
+from flask import Flask, current_app
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
