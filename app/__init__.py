af892c67319b9afd9d290c0ce966f325e188b492
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-02 11:44:58 +0000

    Imports: Error and Mail added

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import Config
 
 db = SQLAlchemy()
@@ -14,6 +15,7 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -21,9 +23,20 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    from app.cli import bp as cli_bp
+    app.register_blueprint(cli_bp)
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
