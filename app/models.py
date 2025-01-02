74db3174bfd2eb6d623a40073d6101b78e75d70c
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-02 19:42:18 +0000

    Support for user tokens.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ User database model"""
 
+from datetime import timedelta
 from flask import current_app, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
@@ -20,6 +21,13 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+    last_message_read_time: so.Mapped[Optional[datetime]]
+    token: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(32), index=True, unique=True)
+    token_expiration: so.Mapped[Optional[datetime]]
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -57,6 +65,20 @@ class User(UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+     def get_token(self, expires_in=3600):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration.replace(
+                tzinfo=timezone.utc) > now + timedelta(seconds=60):
+            return self.token
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.now(timezone.utc) - timedelta(
+            seconds=1)
 
     @staticmethod
     def verify_reset_password_token(token):
