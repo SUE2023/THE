f94d8dc018e9e922c8e70dcbd7282f53f83b0f6e
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-13 18:36:06 +0000

    MongoBD Configuration: Added

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Secret key configuration """
+"""Configuration File """
 import os
 from dotenv import load_dotenv
 
@@ -8,10 +8,19 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
+    # General Configuration
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+
+    # SQLAlchemy Configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # MongoDB Configuration
+    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/')
+    MONGODB_DB_NAME = os.environ.get('MONGODB_DB_NAME', 'mydatabase')
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
