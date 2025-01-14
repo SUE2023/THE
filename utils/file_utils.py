b233491b5a440c4ad48c7603277e1eb00de970bc
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-14 05:09:17 +0000

    Styled Directory

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Utility functions related to file handling """
+
+
 def allowed_file(filename, allowed_extensions):
     """
     Check if the file extension is allowed.
@@ -7,4 +9,4 @@ def allowed_file(filename, allowed_extensions):
     :param allowed_extensions: Set of allowed file extensions
     :return: True if the file extension is allowed, False otherwise
     """
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_extensions
