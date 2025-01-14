f657332ad614548331125c3d2787c7966c0d447f
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-14 05:15:47 +0000

    Importation of Utility Function

--- THE.py
+++ THE.py
@@ -5,12 +5,15 @@ import sqlalchemy.orm as so
 from app import create_app, db
 from app.models import User
 from app import cli
+from utils.file_utils import allowed_file
 
 app = create_app()
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User}
+    return {"sa": sa, "so": so, "db": db, "User": User}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
